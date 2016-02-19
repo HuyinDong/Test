b4663f119e7fa6e90addb89d9d46641b416ea298
huyin <dejorkoffe@gmail.com>
2016-02-19 18:37:50 -0500

    add exploit

--- ajax/tableAndChart/getTable.php
+++ ajax/tableAndChart/getTable.php
@@ -4,6 +4,7 @@ require_once("../db.php");
 $vendor = $_GET['vendor'];
 $product = $_GET['product'];
 $version = $_GET['version'];
+$limit = $_GET['limits'];
 /*$sql = "SELECT * FROM `vendor` WHERE vendor_name = '".$vendor.
        "' and prod_name = '".$product."' and vers_num LIKE '%".$version."%'";
 */
@@ -11,17 +12,23 @@ $version = $_GET['version'];
 $sql = "SELECT a.vers_num, a.edition, c.cvename from vendor a
         inner join cve_vendor b on a .vendor_id = b.vendor_id
         inner join CVEs c on b.cve_id = c.cve_id
-        where vendor_name = '".$vendor."' and prod_name = '".$product."' and vers_num  LIKE '%".$version."%'";
+        where vendor_name = '".$vendor."' and prod_name = '".$product."' and vers_num
+        LIKE '%".$version."%'
+        order by vers_num,cvename asc
+  			limit   $limit"  ;
 $results  = mysql_query($sql) or die("Query failed: " . mysql_error());
 
 $versions = array();
+$data = array();
 
 if ($results) {
-    while ($row = mysql_fetch_object($results)){
+    while ($row = mysql_fetch_object($results))
       $versions[] = $row;
-      }
+
 }
+
+$data['data'] = $versions;
 //echo $sql;
-echo json_encode($versions);
+echo json_encode($data);
 
  ?>
